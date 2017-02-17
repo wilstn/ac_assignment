91fe6e116a51a143b17c2d0068283d08ea299e3f
icebear <wt@wtcom.local>
2017-02-17 13:49:05 +0800

    Add methods to posts controller

--- emergency_report_system/app/controllers/posts_controller.rb
+++ emergency_report_system/app/controllers/posts_controller.rb
@@ -1,2 +1,50 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:headline, :content, :category)
+  end
 end
