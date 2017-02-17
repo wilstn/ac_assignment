dcc0e550fb9e174cb87edc40fab2362efd784b6b
icebear <wt@wtcom.local>
2017-02-17 14:03:44 +0800

    Add comments controller

--- emergency_report_system/app/controllers/comments_controller.rb
+++ emergency_report_system/app/controllers/comments_controller.rb
@@ -1,2 +1,55 @@
 class CommentsController < ApplicationController
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.all
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :entry, :post_id)
+  end
 end
