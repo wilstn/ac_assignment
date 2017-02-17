818d9f63d47adc582abaa424fbd2ab788751bb6d
icebear <wt@wtcom.local>
2017-02-17 13:53:10 +0800

    Add post and comments associactions in model

--- emergency_report_system/app/controllers/posts_controller.rb
+++ emergency_report_system/app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    if @post.update
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render 'edit'
